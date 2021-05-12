e7a0dfa8fbec7e58078e7774349a062e5b73c95d
Garrett Allen <neelik.n33l@gmail.com>
2021-05-12 14:47:39 -0600

    Updated scripts to work with a Google Colab notebook.

--- agnews.sh
+++ agnews.sh
@@ -1,6 +1,6 @@
 
 export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
 DATASET=agnews
 LABEL_NAME_FILE=label_names.txt
@@ -9,13 +9,14 @@ TEST_CORPUS=test.txt
 TEST_LABEL=test_labels.txt
 MAX_LEN=200
 TRAIN_BATCH=32
-ACCUM_STEP=2
+ACCUM_STEP=4
 EVAL_BATCH=128
-GPUS=2
+GPUS=1
 MCP_EPOCH=3
 SELF_TRAIN_EPOCH=1
+COLAB=/content/LOTClass/
 
-python src/train.py --dataset_dir datasets/${DATASET}/ --label_names_file ${LABEL_NAME_FILE} \
+python ${COLAB}src/train.py --dataset_dir ${COLAB}datasets/${DATASET}/ --label_names_file ${LABEL_NAME_FILE} \
                     --train_file ${TRAIN_CORPUS} \
                     --test_file ${TEST_CORPUS} --test_label_file ${TEST_LABEL} \
                     --max_len ${MAX_LEN} \
